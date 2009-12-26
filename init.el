4d5fbe8f6742b4433a991c081d59dde4f2ebe498
Sho HASHIMOTO <sho-h@netlab.jp>
2009-12-26 23:28:14 +0900

    loadするファイルの整理。

--- init.el
+++ init.el
@@ -8,13 +8,25 @@
   "~/.emacs.d/conf" "~/.emacs.d/elisp" "~/.emacs.d/auto-install")
 
 (load "init-japanese")
-(load "init-keybind")
-(load "init-ruby")
+(load "init-basis")
 (load "init-c")
+(load "init-ruby")
+(load "init-js2")
 (load "init-gpg")
 (load "init-wl")
 (load "init-eshell")
+(load "init-svn")
+(load "init-whitespace")
 (load "init-auto-install")
 (load "anything-startup")
 (load "init-anything-rurema")
 (load "init-desktop")
+(load "dired-setup")
+
+; タブと全角スペースの見た目をわかりやすいものにする。
+; c.f. http://homepage3.nifty.com/satomii/software/elisp.ja.html
+(require 'jaspace)
+
+; 日本語のインクリメンタル検索を行う。
+; migemo と一緒にインストールできる。
+(load "migemo/migemo")
